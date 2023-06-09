66d2aa21cba25a29391ee1b0ce73d1554528f656
Isaque Andrade <90810299+oisaque@users.noreply.github.com>
2023-06-09 10:15:00 -0300

    Create Project HTML

---
+++ WebSiteCurso/js/script.js
@@ -0,0 +1,6 @@
+function displayon() {
+    document.getElementById("form").style.visibility = "visible"
+}
+function displayoff() {
+    document.getElementById("form").style.visibility = "hidden"
+}
