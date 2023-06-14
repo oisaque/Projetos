c068c8e4acc4a35d84b5d23c312c1adf12556927
Isaque Andrade <90810299+oisaque@users.noreply.github.com>
2023-06-14 12:57:44 -0300

    JavaScript

---
+++ JavaScript Ver Mais/main.js
@@ -0,0 +1,38 @@
+const cards = document.querySelectorAll(".card");
+
+//Disable see more function from the text contents that the height is less than the minimum visible height of a text content on page load.
+window.onload = function(){
+    cards.forEach((card) => {
+        let seeMoreBtn = card.querySelector(".see-more-btn");
+        let textContent = card.querySelector(".card-content .text");
+
+        if(textContent.scrollHeight == textContent.clientHeight){
+            seeMoreBtn.style.display = "none";
+            textContent.style.height = "fit-content";
+        }
+        else{
+            card.classList.add("gradient");
+        }
+    });
+}
+
+//See more function on click the see more button on each card.
+cards.forEach((card) => {
+    let seeMoreBtn = card.querySelector(".see-more-btn");
+    let textContent = card.querySelector(".card-content .text");
+
+    seeMoreBtn.addEventListener("click", () => {
+        card.classList.toggle("active");
+        card.classList.toggle("gradient");
+
+        if(card.classList.contains("active")){
+            seeMoreBtn.innerHTML = "See Less";
+            textContent.style.height = textContent.scrollHeight + "px";
+        }
+        else{
+            seeMoreBtn.innerHTML = "See More";
+            textContent.style.height = "100px";
+        }
+    });
+});
+      
